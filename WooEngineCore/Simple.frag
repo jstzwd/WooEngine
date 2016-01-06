980f44cd2caedf547c70e70e88d8142cb64d695b
Woody Wu <jstzwd@gmail.com>
2016-01-05 17:25:14 -0800

    Added shader and rendering testing code.

--- WooEngineCore/Simple.frag
+++ WooEngineCore/Simple.frag
@@ -3,5 +3,5 @@
 layout(location=0) out vec4 color;
 
 void main(){
-    color=vec4(1.f,0.f,1.f,0.f);
+    color=vec4(1.f,0.f,1.f,1.f);
 }
--- WooEngineCore/main.cpp
+++ WooEngineCore/main.cpp
@@ -12,14 +12,37 @@ int main()
 
 	Window window("WooEngine",800,600);
 	glClearColor(0.f, 0.f, 1.f, 1.f);
+	/*
 	GLuint vao;
 	glGenVertexArrays(1,&vao);
 	glBindVertexArray(vao);
 	glGetString(GL_SHADING_LANGUAGE_VERSION);
+	*/
+	/*
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.5, 0.5, 0);
+	glVertex3f(-0.5, 0, 0);
+	glVertex3f(0, -0.5, 0);
+	glEnd();
+	*/
+	GLfloat vertices[] = {
+		0,-1,0,
+		1,0,0,
+		0,0,0
+	};
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
 	Shader myShader("Simple.vert", "Simple.frag");
+	myShader.Enable();
+	
 	while (!window.Closed()) {
 		window.Clear();
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 9);
 		window.Update();
 	}
 	return 0;
